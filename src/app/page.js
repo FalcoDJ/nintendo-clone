15a12f4266bc6f709b528ec94a8cfd3169bcb78e
FalcDJ <retrotitanx3@gmail.com>
2023-07-25 01:52:35 -0400

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,145 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { createClient } from "contentful";
+import Image from "next/image";
 
-export default function Home() {
+const client = createClient({
+  space: process.env.CONTENT_SPACE_ID,
+  accessToken: process.env.CONTENT_DELIVERY_TOKEN,
+});
+
+async function getContent() {
+  const entries = await client.getEntries({
+    limit: 1,
+    content_type: "landingPage",
+  });
+
+  const { items } = entries;
+
+  return items;
+}
+
+function compressToFields(obj) {
+  obj.hasOwnProperty("metadata") && delete obj["metadata"];
+  obj.hasOwnProperty("sys") && delete obj["sys"];
+
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] == "object") {
+      obj[key] = compressToFields(obj[key]);
+    }
+  });
+
+  return { ...(obj.fields || obj) };
+}
+
+export default async function Landing() {
+  const page = await getContent();
+
+  const elements = compressToFields(page)[0];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className="hero">
+        <div
+          className="hero-bg"
+          style={{
+            backgroundImage:
+              "url(https:" + elements.heroBanner.background.file.url + ")",
+          }}
+        ></div>
+        <div className="hero-content">
+          <img
+            src={"https:" + elements.heroBanner.banner.file.url}
+            // {...elements.heroBanner.banner.file.details.image}
+            alt="banner"
+          />
+          <span>
+            <h1>{elements.heroBanner.title}</h1>
+            <a href="#" className="cta-btn">
+              {elements.heroBanner.ctaButton}
+            </a>
+          </span>
         </div>
-      </div>
+      </section>
+      {elements.featured && (
+        <>
+          <section className="featured">
+            <div className="section-content">
+              <h1>Featured</h1>
+              <img
+                src={"https:" + elements.featured.banner.file.url}
+                alt="pikmin-feature-banner"
+              />
+              <span>
+                <h3>{elements.featured.hookText}</h3>
+                <a href="#" className="cta-btn">
+                  {elements.featured.ctaButton}
+                </a>
+              </span>
+              <div className="game-list">
+                {elements.freshGames && (
+                  <>
+                    {Object.keys(elements.freshGames).map((key) => {
+                      const card = elements.freshGames[key];
+                      return (
+                        <>
+                          <a href="#" className="game-card">
+                            <div className="game-card-img-wrapper">
+                              <img
+                                src={"https:" + card.thumbnail.file.url}
+                                alt={card.titleSlug}
+                              />
+                            </div>
+                            <div className="game-card-info">
+                              <h3>{card.title}</h3>
+                              <span className="platform">
+                                <p>{card.platform}</p>
+                              </span>
+                            </div>
+                          </a>
+                        </>
+                      );
+                    })}
+                  </>
+                )}
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+      {elements.characters && (
+        <>
+          <section className="characters">
+            <div className="section-content">
+              <h1>Characters</h1>
+              <div className="character-grid">
+                {Object.keys(elements.characters).map((key) => {
+                  const character = elements.characters[key];
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                  return (
+                    <div className="character-card">
+                      <a href="#" className="image-wrapper">
+                        <div className="character-bg">
+                          <img
+                            src={"https:" + character.background.file.url}
+                            alt={character.titleSlug + " background"}
+                          />
+                        </div>
+                        <div className="character-mug">
+                          <img
+                            src={"https:" + character.character.file.url}
+                            alt={character.titleSlug + " mug"}
+                          />
+                        </div>
+                      </a>
+                      <h1>{character.name}</h1>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        </>
+      )}
+      {/* {JSON.stringify(elements)} */}
     </main>
-  )
+  );
 }
